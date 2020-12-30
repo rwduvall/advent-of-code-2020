db5de2bb84e1bc2663cda9641cfdaa6889182eed
Robbie Duvall <robbie.duvall@willowtreeapps.com>
2020-12-30 15:16:39 -0500

    Almost to something that works

--- src/day13.ts
+++ src/day13.ts
@@ -27,3 +27,41 @@ export const mutlipleUntil = (item: number, greatThanOrEqual: number) => {
     }
     return result
 }
+
+export const part2 = (input: string) => {
+    const parsedInput = input.split(',').map(i => {
+        if (i != 'x') {
+            return Number(i)
+        } else {
+            return 'x'
+        }
+    }).map((n, index) => {
+        if (n != 'x') {
+            return { 'busNumber': n, 't': index }
+        }
+    }).filter(item => {
+        return item !== undefined
+    })
+
+    let firstBus = parsedInput[0].busNumber
+    let secondBus = parsedInput[1].busNumber
+    const secondBusT = parsedInput[1].t
+
+    const foo = (secondBus - secondBusT) / firstBus
+    let stop = true
+    // console.log({ foo })
+
+    // while (secondBus < 1068780) {
+    while (stop) {
+        // do {
+        secondBus += parsedInput[1].busNumber
+        if (Number.isInteger((secondBus - secondBusT) / firstBus) == true) {
+            stop = false
+        }
+    }
+
+    console.log(firstBus)
+    console.log(secondBus)
+
+    return parsedInput
+}
